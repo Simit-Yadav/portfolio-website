bea1cd0f494cdbe86f2443c7a85086296603a73d
Simit-Yadav <sim5394yadav@hgmail.com>
2021-07-05 21:04:34 -0400

    skills-sec

--- static/js/app.js
+++ static/js/app.js
@@ -8,4 +8,17 @@ window.addEventListener("DOMContentLoaded", () => {
     overlay.classList.toggle("show-nav");
     body.classList.toggle("no-scroll");
   });
+
+  let percentage = document.querySelectorAll(".line-bar");
+  console.log(percentage);
+  percentage.forEach((value) => {
+    var line = new ProgressBar.Line(value, {
+      color: "#f8bd3b",
+      trailColor: "#F5F9FF",
+      strokeWidth: 3.1,
+      duration: 2000,
+      easing: "easeInOut",
+    });
+    line.animate(value.dataset.percentage / 100);
+  });
 });
